4e1e72031f275ee6eb3be3e68310c16e13885cb4
Poornajith <chiranthapoornajith@gmail.com>
2024-08-30 13:12:50 +0530

    slider

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -4,40 +4,42 @@ import '../index.css'
 import '../App.css'
 import {Button} from "react-bootstrap";
 import {useNavigate} from "react-router-dom";
-import { getLabelText } from './MultipleLanguageSheets'
 export default function Slider() {
-    let templatetype = 'slider'
     const navigate = useNavigate()
     return(
         <div className={'slider'}>
             <Carousel showThumbs={false} autoPlay={true} infiniteLoop={true}>
                 <div className="p-5 slider-div slider-div-1 text-white">
-                    <h1>{getLabelText('Elevate your business with our', templatetype)} <br /> {getLabelText('Cutting-edge Software', templatetype)}</h1>
-                    <h6>{getLabelText('Software Products and Custom Development Solutions', templatetype)} </h6>
-                        < Button className = { 'm-1 btn-pink'} onClick = {()=> navigate('/products')}>{getLabelText('Explore Products', templatetype)} </Button>
-                    < Button className={'m-1 btn-pink'} onClick={() => navigate('/projects')}>{getLabelText('Start Your Dressing', templatetype)} </Button>
+                    <div className="slider-txt mt-3 py-2">
+                        <h1>Elevate Your Beauty. Elevate Your Confidence.</h1>
+                        <h6>Experience the epitome of luxury beauty. Elevate your style, elevate your confidence.</h6>
+                        < Button className={'m-1 btn-pink'} onClick={() => navigate('/contactus')}>Schedule an
+                            Occasion</Button>
+                    </div>
                 </div>
-                <div className={'p-5 slider-div slider-div-2 text-white align-items-center'}>
-                    <h1 className={'mt-5'}>{getLabelText('Enlightening Your Future With Our Solutions', templatetype)}</h1>
-                        < h6 > {
-                        getLabelText('Contact Us Today !', templatetype)}</h6>
-                    <Button className={'m-1 btn-pink'} onClick={() => navigate('/contactus')}>{getLabelText('Contact us', templatetype)}</Button>
+                <div className={'p-5 slider-div slider-div-2 text-white'}>
+                    <div className="slider-txt mt-3 py-2">
+                        <h1>Transform Your Look</h1>
+                        <h6>Tailored beauty solutions just for you. Your vision, our expertise.</h6>
+                        <Button className={'m-1 btn-pink'} onClick={() => navigate('/contactus')}>Contact Us</Button>
+                    </div>
                 </div>
                 <div className={'p-5 slider-div slider-div-3 text-white'}>
-                    <h1 className={'mt-5'}>{getLabelText('Innovative Software Tailored to Your Needs', templatetype)}</h1>
-                        < h6 > {
-                        getLabelText('We understand that every business is unique.', templatetype)} <br/>
-                        {getLabelText('That\'s why we offer custom software development to meet your specific requirements.', templatetype)}
-                    </h6>
-                    <Button className={'m-1 btn-pink'} onClick={() => navigate('/products')}>{getLabelText('Explore Products', templatetype)}</Button>
+                    <div className="slider-txt mt-3 py-2">
+                        <h1>Relax, Renew, Rejuvenate</h1>
+                        < h6> Achieve the look you've always dreamed of. Your beauty goals, realized.</h6>
+                        <Button className={'m-1 btn-pink'} onClick={() => navigate('/contactus')}>Let's Do it</Button>
+                    </div>
                 </div>
                 <div className={'p-5 slider-div slider-div-4 text-white'}>
-                    <h1 className={'mt-5'}>{getLabelText('Your Vision, Our Expertise: Custom Software Development', templatetype)}</h1>
-                        < h6 > {
-                        getLabelText('Have a unique idea? Let\'s bring it to life.', templatetype)} <br/>
-                                { getLabelText('Our skilled developers can create bespoke software solutions that align perfectly with your business goals.', templatetype)}
-                    </h6>
-                    <Button className={'m-1 btn-pink'} onClick={() => navigate('/projects')}>{getLabelText('Start Your Dressing', templatetype)}</Button>
+                    <div className="slider-txt mt-3 py-2">
+                        <h1>Reimagine Your Beauty</h1>
+                        < h6>
+                            Discover your unique beauty potential. We'll help you shine.
+                        </h6>
+                        <Button className={'m-1 btn-pink'} onClick={() => navigate('/contactus')}>Start Your
+                            Dressing</Button>
+                    </div>
                 </div>
             </Carousel>
         </div>
